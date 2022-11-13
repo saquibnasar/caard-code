4fb9795a11beefd9d911243cf175b72a7428fc35
Saquib Siddiqui <saquibsiddiqui023@gmail.com>
2022-11-13 15:04:40 +0530

    pdf viewer changes

--- src/component/Documents.js
+++ src/component/Documents.js
@@ -10,6 +10,7 @@ import { ScrollMode } from "@react-pdf-viewer/core";
 export default function Documents({ data }) {
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPage, setTotalPage] = useState(1);
+  const [scrollLeft, setScrollLeft] = useState(1);
 
   const pageNavigationPluginInstance = pageNavigationPlugin();
 
@@ -21,26 +22,68 @@ export default function Documents({ data }) {
   const goToNextPage = (currentPage, totalPage) => {
     if (pdfHolder && !(currentPage === totalPage)) {
       const pdfNum = pdf[0].style.width.split("px")[0];
-      const pdfLeft = pdfHolder.scrollLeft;
-      pdfHolder.scrollLeft = pdfLeft + parseFloat(pdfNum);
+      pdfHolder.scrollLeft = currentPage * parseFloat(pdfNum);
       setCurrentPage(currentPage + 1);
+      setScrollLeft(currentPage * parseFloat(pdfNum));
     }
   };
 
   const goToPreviousPage = (currentPage) => {
     if (pdfHolder && !(currentPage === 1)) {
       const pdfNum = pdf[0].style.width.split("px")[0];
-      if (currentPage === 2) {
-        pdfHolder.scrollLeft = parseFloat(pdfNum);
-      }
-      const pdfLeft = pdfHolder.scrollLeft;
-      pdfHolder.scrollLeft = pdfLeft - parseFloat(pdfNum);
+      pdfHolder.scrollLeft = scrollLeft - parseFloat(pdfNum);
+      setScrollLeft(scrollLeft - parseFloat(pdfNum));
       setCurrentPage(currentPage - 1);
     }
   };
+
   const totalPageNumber = (pageNum) => {
     setTotalPage(pageNum);
   };
+
+  // document.addEventListener("touchstart", handleTouchStart, false);
+  // document.addEventListener("touchmove", handleTouchMove, false);
+
+  // var xDown = null;
+  // var yDown = null;
+
+  // function handleTouchStart(evt) {
+  //   xDown = evt.touches[0].clientX;
+  //   yDown = evt.touches[0].clientY;
+  // }
+
+  // function handleTouchMove(evt) {
+  //   if (!xDown || !yDown) {
+  //     return;
+  //   }
+
+  //   var xUp = evt.touches[0].clientX;
+  //   var yUp = evt.touches[0].clientY;
+
+  //   var xDiff = xDown - xUp;
+  //   var yDiff = yDown - yUp;
+
+  //   if (Math.abs(xDiff) > Math.abs(yDiff)) {
+  //     /*most significant*/
+  //     if (xDiff > 0) {
+  //       /* left swipe */
+  //       // alert("left swipe");
+  //       goToNextPage(currentPage, totalPage);
+  //     } else {
+  //       /* right swipe */
+  //       goToNextPage(currentPage, totalPage);
+  //     }
+  //   } else {
+  //     if (yDiff > 0) {
+  //       /* up swipe */
+  //     } else {
+  //       /* down swipe */
+  //     }
+  //   }
+  //   /* reset values */
+  //   xDown = null;
+  //   yDown = null;
+  // }
 
   return (
     <>
